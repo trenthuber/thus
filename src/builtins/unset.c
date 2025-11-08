7e8b3137e7cc5e13005a4c0545b840b09a530838
Trent Huber <trentmhuber@gmail.com>
2025-11-08 03:10:13 -0500

    Update behavior of unset builtin

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,10 +6,6 @@
 BUILTIN(unset) {
 	if (argc != 2) return usage(argv[0], "name");
 
-	if (!getenv(argv[1])) {
-		note("$%s$ does not exist", argv[1]);
-		return EXIT_FAILURE;
-	}
 	if (unsetenv(argv[1]) == -1) {
 		note("Unable to unset $%s$", argv[1]);
 		return EXIT_FAILURE;
