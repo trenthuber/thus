8d7027fb973df28119cf27c001cca735d7b26144
Trent Huber <trentmhuber@gmail.com>
2025-11-24 00:14:27 -0500

    pwd builtin works when current directory changes name

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -13,19 +13,17 @@ int pwd(char **args, size_t numargs) {
 
 	if (numargs != 1) return usage(args[0], NULL);
 
-	if (!(cwd = getenv("PWD"))) {
-		if (!(cwd = getcwd(buffer, PATH_MAX))) {
-			note("Unable to get current working directory");
-			return EXIT_FAILURE;
-		}
-		if (buffer[(l = strlen(buffer)) - 1] != '/') {
-			buffer[l++] = '/';
-			buffer[l] = '\0';
-		}
-		if (setenv("PWD", buffer, 1) == -1) {
-			note("Unable to set $PWD$");
-			return EXIT_FAILURE;
-		}
+	if (!(cwd = getcwd(buffer, PATH_MAX))) {
+		note("Unable to get current working directory");
+		return EXIT_FAILURE;
+	}
+	if (buffer[(l = strlen(buffer)) - 1] != '/') {
+		buffer[l++] = '/';
+		buffer[l] = '\0';
+	}
+	if (setenv("PWD", buffer, 1) == -1) {
+		note("Unable to set $PWD$");
+		return EXIT_FAILURE;
 	}
 
 	puts(cwd);
